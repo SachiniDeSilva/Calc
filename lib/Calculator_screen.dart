0c3be6f25cfb6819c5cefcf94ff70d14b799409a
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2023-12-18 18:51:01 +0530

    change 1

--- lib/Calculator_screen.dart
+++ lib/Calculator_screen.dart
@@ -2,10 +2,13 @@
 import 'package:flutter/material.dart';
 
 class Calculator extends StatefulWidget{
+  const Calculator({super.key});
+
   @override
   State<Calculator> createState() => _CalculatorState();
 }
 class _CalculatorState extends State<Calculator> {
+  // ignore: non_constant_identifier_names
   String UserInput ="";
   String result ="0";
 
@@ -14,16 +17,47 @@ class _CalculatorState extends State<Calculator> {
     ')',
     '(',
     '7',
-    '123'
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'C',
+    '0',
+    '',
+    '=',
   ];
 
 
 
   
   @override
-  Widget build(BuildContext){
-    return const Scaffold(
+  Widget build(BuildContext context){
+    return Scaffold(
       backgroundColor: Colors.black,
+      body: Column(children: [
+        SizedBox(height: MediaQuery.of(context).size.height / 3,
+        child: Column(children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerRight,
+            child: const Text(
+              "Input",
+              style: TextStyle(
+                fontSize: 32,
+                color: Color.fromARGB(255, 248, 248, 248),
+              ),
+            ),
+          )
+        ]),
+        )
+      ],),
     );
   }
   }
