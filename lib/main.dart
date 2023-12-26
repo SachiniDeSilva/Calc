518c17b803aaf980964600f847dce6935930e00a
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2023-12-26 18:38:06 +0530

    Update main.dart
    calculator use simple c

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cal/Calculator_screen.dart';
+import 'package:cal/calculator_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Calculator(),
       
